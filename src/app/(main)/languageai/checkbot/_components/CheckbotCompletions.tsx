7c9231ece0e651256989e4de7242244177b36674
Stanley Winata <winatastanley355@gmail.com>
2024-10-17 15:38:30 +0700

    Prettier

--- src/app/(main)/languageai/checkbot/_components/CheckbotCompletions.tsx
+++ src/app/(main)/languageai/checkbot/_components/CheckbotCompletions.tsx
@@ -15,19 +15,17 @@ const CheckbotCompletions = () => {
   return (
     <Tabs
       titles={completions.map((_, index) => `Variant ${index + 1}`)}
-      contents={
-        completions.map((completion, index) => (
-          <div
-            key={`completionContent${index}`}
-            className="text-sm p-2 flex gap-2 overflow-auto"
-          >
-            <div className="flex-1">{completion}</div>
-            <Button size="icon" onClick={() => copyToClipboard(completion)}>
-              <LuCopy />
-            </Button>
-          </div>
-        ))
-      }
+      contents={completions.map((completion, index) => (
+        <div
+          key={`completionContent${index}`}
+          className="text-sm p-2 flex gap-2 overflow-auto"
+        >
+          <div className="flex-1">{completion}</div>
+          <Button size="icon" onClick={() => copyToClipboard(completion)}>
+            <LuCopy />
+          </Button>
+        </div>
+      ))}
     />
   );
 };
--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,13 +1,13 @@
-'use client'
+"use client";
 
 import { useState, memo } from "react";
 import { Button } from "./button";
 import { cn } from "@/lib/utils";
 
 type TTabs = {
-  titles: string[]
-  contents: React.ReactNode[]
-}
+  titles: string[];
+  contents: React.ReactNode[];
+};
 
 const Tabs = ({ titles, contents }: TTabs) => {
   const [activeTabs, setActiveTabs] = useState(0);
@@ -15,15 +15,31 @@ const Tabs = ({ titles, contents }: TTabs) => {
   return (
     <div>
       <div className="p-1 w-full flex">
-        {titles.map((title, index) =>
-          <Button type="button" className="flex-1 w-full" variant={activeTabs === index ? "default" : "ghost"} onClick={() => setActiveTabs(index)} key={title}>{title}</Button>
-        )}
+        {titles.map((title, index) => (
+          <Button
+            type="button"
+            className="flex-1 w-full"
+            variant={activeTabs === index ? "default" : "ghost"}
+            onClick={() => setActiveTabs(index)}
+            key={title}
+          >
+            {title}
+          </Button>
+        ))}
       </div>
-      {contents.map((content, index) => <div key={`tabs_content_${index}`} className={cn("w-full h-full overflow-x-hidden", activeTabs === index ? "block" : "hidden")}>
-        {content}
-      </div>)}
+      {contents.map((content, index) => (
+        <div
+          key={`tabs_content_${index}`}
+          className={cn(
+            "w-full h-full overflow-x-hidden",
+            activeTabs === index ? "block" : "hidden",
+          )}
+        >
+          {content}
+        </div>
+      ))}
     </div>
-  )
+  );
 };
 
 export default memo(Tabs);
