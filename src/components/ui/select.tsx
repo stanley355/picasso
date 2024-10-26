843409318821edffd3eebabc2bffff53d5a65953
installer <installer@gmail.com>
2024-10-26 14:09:28 +0700

    feat: Update select component and remove tabs

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -2,15 +2,11 @@
 
 import * as React from "react";
 import * as SelectPrimitive from "@radix-ui/react-select";
-import { LuCheck, LuChevronDown, LuChevronUp } from "react-icons/lu";
+import { LuChevronDown, LuChevronUp } from "react-icons/lu";
 
 import { cn } from "@/lib/utils";
 
 const Select = SelectPrimitive.Root;
-
-const SelectGroup = SelectPrimitive.Group;
-
-const SelectValue = SelectPrimitive.Value;
 
 const SelectTrigger = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>,
