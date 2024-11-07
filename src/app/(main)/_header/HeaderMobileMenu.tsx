bb0ac59d12bf2cd5209492a7ed51f980717ca9d3
installer <installer@gmail.com>
2024-11-07 11:03:41 +0700

    Prettier

--- src/app/(main)/_header/HeaderMobileMenu.tsx
+++ src/app/(main)/_header/HeaderMobileMenu.tsx
@@ -1,47 +1,42 @@
 "use client";
-import {useShallow} from "zustand/shallow";
-import {useHeaderStore} from "./_store/useHeaderStore";
-
+import { useShallow } from "zustand/shallow";
+import { useHeaderStore } from "./_store/useHeaderStore";
 
 import LanguageSidebarMenu from "../_sidebar/LanguageSidebarMenu";
 import SupportSidebarMenu from "../_sidebar/SupportSidebarMenu";
 import AccountSidebarMenu from "../_sidebar/AccountSidebarMenu";
 import BpsSidebarMenu from "@/app/(main)/_sidebar/BpsSidebarMenu";
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const HeaderMobileMenu = () => {
-    const {showMobileMenu} = useHeaderStore(
-        useShallow((state) => ({
-            showMobileMenu: state.showMobileMenu,
-        })),
-    );
+  const { showMobileMenu } = useHeaderStore(
+    useShallow((state) => ({
+      showMobileMenu: state.showMobileMenu,
+    })),
+  );
 
-    if (!showMobileMenu) return null;
+  if (!showMobileMenu) return null;
 
-    return (
-        <div className="w-full h-full absolute top-0 left-0 bg-foreground p-2">
-            <Tabs defaultValue="0">
-                <TabsList>
-                    <TabsTrigger value="0">
-                        AI
-                    </TabsTrigger>
-                    <TabsTrigger value="1">
-                        Account
-                    </TabsTrigger>
-                </TabsList>
-                <TabsContent value="0" className="mt-4">
-                    <div className="flex flex-col gap-2">
-                        <LanguageSidebarMenu/>
-                        <BpsSidebarMenu/>
-                        <SupportSidebarMenu/>
-                    </div>
-                </TabsContent>
-                <TabsContent value="1" className="mt-4">
-                    <AccountSidebarMenu/>
-                </TabsContent>
-            </Tabs>
-        </div>
-    );
+  return (
+    <div className="w-full h-full absolute top-0 left-0 bg-foreground p-2">
+      <Tabs defaultValue="0">
+        <TabsList>
+          <TabsTrigger value="0">AI</TabsTrigger>
+          <TabsTrigger value="1">Account</TabsTrigger>
+        </TabsList>
+        <TabsContent value="0" className="mt-4">
+          <div className="flex flex-col gap-2">
+            <LanguageSidebarMenu />
+            <BpsSidebarMenu />
+            <SupportSidebarMenu />
+          </div>
+        </TabsContent>
+        <TabsContent value="1" className="mt-4">
+          <AccountSidebarMenu />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default HeaderMobileMenu;
